9d3f9018480910bfb77e116f26924d9478ef232c
Abdulsamii Ajala <kgasta@gmail.com>
2024-07-13 11:19:19 +0100

    feat: steps counter

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 var count = 0.obs;
+var steps = 1.obs;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,23 +14,47 @@ class HomeScreen extends StatelessWidget {
           body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text('Count ${count.value}'),
             const SizedBox(
               height: 10,
             ),
-            ElevatedButton(
-              onPressed: () {
-                count.value = count.value + 1;
-              },
-              child: const Text('Increase'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    count.value = count.value - steps.value;
+                  },
+                  child: const Text('Decrease'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    count.value = steps.value + count.value;
+                  },
+                  child: const Text('Increase'),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                count.value = count.value + 1;
-              },
-              child: const Text('Increase'),
-            )
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    steps.value--;
+                  },
+                  child: const Text('Decrease Step'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    steps.value++;
+                  },
+                  child: const Text('Increase Step'),
+                ),
+              ],
+            ),
+            Text('Steps ${steps.value}'),
           ],
         ),
       ));
