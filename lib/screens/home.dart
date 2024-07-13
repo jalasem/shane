122c4bc586179923461bf70fe8a62e517cc81867
Abdulsamii Ajala <kgasta@gmail.com>
2024-07-13 11:29:31 +0100

    feat: counter app with Getx Controller

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-var count = 0.obs;
-var steps = 1.obs;
+class HomeScreenController extends GetxController {
+  var count = 0.obs;
+
+  void increment() {
+    count.value++;
+  }
+
+  void decrement() {
+    count.value--;
+  }
+}
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final controller = Get.put(HomeScreenController());
 
   @override
   Widget build(BuildContext context) {
     return Obx(() {
+      final count = controller.count.value;
+
       return Scaffold(
           body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Count ${count.value}'),
+            Text('Count $count'),
             const SizedBox(
               height: 10,
             ),
@@ -24,37 +37,15 @@ class HomeScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                  onPressed: () {
-                    count.value = count.value - steps.value;
-                  },
+                  onPressed: controller.decrement,
                   child: const Text('Decrease'),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    count.value = steps.value + count.value;
-                  },
+                  onPressed: controller.increment,
                   child: const Text('Increase'),
                 ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    steps.value--;
-                  },
-                  child: const Text('Decrease Step'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    steps.value++;
-                  },
-                  child: const Text('Increase Step'),
-                ),
-              ],
-            ),
-            Text('Steps ${steps.value}'),
           ],
         ),
       ));
